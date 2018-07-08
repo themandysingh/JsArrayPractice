a2975ca859d8e9a1cc1f8b1f77f398e2f4ff2046
themandysingh <mandy98@outlook.com>
2018-07-08 10:00:24 +0530

    exercise complete

--- JsArrayPractice.js
+++ JsArrayPractice.js
@@ -46,9 +46,27 @@
         console.table(oldest);
     // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
     // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+        // const links = [...document.querySelectorAll(".mw-category a")];
+        // const de = links
+        //             .map(link => link.textContent)
+        //             .filter(name => name.includes("de"));
     // 7. sort Exercise
     // Sort the people alphabetically by last name
+        const sortedByLastName = people.sort((a, b) => {
+            const [aLast, aFirst] = a.split(", ");
+            const [bLast, bFirst] = b.split(", ");
+            return aLast > bLast ? 1 : -1;
+        });
+        console.log(sortedByLastName);
     // 8. Reduce Exercise
     // Sum up the instances of each of these
-    const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];    
+    const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+        const vehicles = data.reduce(function (obj, vehicle) {
+            if(!obj[vehicle]) {
+                obj[vehicle] = 0;
+            }
+            obj[vehicle]++;
+            return obj;
+        },{});
+        console.log(vehicles);
 })();
